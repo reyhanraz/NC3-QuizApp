a6bebd4374218c6ded7e8903fd53690cc7405492
Reyhan Rifqi <reyhanazzami@gmail.com>
2020-06-07 22:55:14 +0700

    Initial Commit

---
+++ NC3-QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NC3-QuizApp
+//
+//  Created by Reyhan Rifqi on 07/06/20.
+//  Copyright © 2020 Reyhan Rifqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
