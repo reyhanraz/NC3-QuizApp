14a0881aeb3a9c929d9793c4bc73601c12759f4f
feidora <feidora5@gmail.com>
2020-06-15 00:49:44 +0700

    added onboarding storyboard and how to play storyboard
    belum disusun dan digabungin sama storyboard lainnya

---
+++ NC3-QuizApp/HowToPlayVC.swift
@@ -0,0 +1,21 @@
+//
+//  HowToPlayVC.swift
+//  NC3-QuizApp
+//
+//  Created by Feidora Nadia Putri on 15/06/20.
+//  Copyright © 2020 Reyhan Rifqi. All rights reserved.
+//
+
+import UIKit
+
+class HowToPlayVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        hideNavigationBar()
+    }
+  func hideNavigationBar() {
+      self.navigationController?.setNavigationBarHidden(true, animated: true)
+  }
+
+}
--- NC3-QuizApp/onBoardingVC.swift
+++ NC3-QuizApp/onBoardingVC.swift
@@ -12,19 +12,12 @@ class onBoardingVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        hideNavigationBar()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func hideNavigationBar() {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
-    */
 
 }
